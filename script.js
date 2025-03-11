46e720a250759052bf7b2522843063e4217e56a9
jansloupensky <jan.sloupensky1@seznam.cz>
2025-03-11 22:01:27 +0100

    secrets file

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu-button");
-    const pinModal = document.getElementById("pin-modal");
-    const pinInput = document.getElementById("pin-input");
-    const pinSubmit = document.getElementById("pin-submit");
-    const menu = document.getElementById("menu");
+// Elementy
+const pinModal = document.getElementById("pin-modal");
+const pinInput = document.getElementById("pin-input");
+const pinSubmit = document.getElementById("pin-submit");
+const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu-button");
 
-    const correctPin = "1234"; // Nastavení pinu
-    // Po kliknutí na tlačítko menu zobrazit modal
-    menuButton.addEventListener("click", function () {
-        pinModal.style.display = "flex";
+// Skrytí menu při načítání stránky
+menu.classList.add("hidden");
+
+let correctPin;
+
+// Načtení hesla ze souboru
+fetch('secrets.json')
+    .then(response => response.json())
+    .then(data => {
+        correctPin = data.correctPin;
+    })
+    .catch(error => {
+        console.error("Chyba při načítání konfiguračního souboru:", error);
     });
 
-    // Ověření PINu
-    pinSubmit.addEventListener("click", function () {
-        if (pinInput.value === correctPin) {
-            pinModal.style.display = "none"; // Zavřít modal
-            menu.classList.toggle("hidden"); // Přepnout viditelnost menu
-        } else {
-            alert("Špatný PIN!");
-            pinInput.value = "";
-        }
-    });
+// Otevření modálního okna při kliknutí na tlačítko menu
+menuButton.addEventListener("click", function () {
+    pinModal.style.display = "block"; 
+});
 
-    // Zavření modalu při kliknutí mimo něj
-    window.addEventListener("click", function (event) {
-        if (event.target === pinModal) {
-            pinModal.style.display = "none";
-        }
-    });
+// Zavření modálního okna při kliknutí mimo něj
+window.addEventListener("click", function (event) {
+    if (event.target === pinModal) {
+        pinModal.style.display = "none";
+    }
+});
 
-    // Zavření menu při kliknutí mimo
-    document.addEventListener("click", function (event) {
-        if (!menu.contains(event.target) && event.target !== menuButton) {
-            menu.classList.add("hidden");
-        }
-    });
+// Ověření PINu
+pinSubmit.addEventListener("click", function () {
+    if (pinInput.value === correctPin) {
+        pinModal.style.display = "none"; 
+        menu.classList.remove("hidden"); 
+    } else {
+        alert("Špatný PIN!");
+        pinInput.value = ""; 
+    }
 });
