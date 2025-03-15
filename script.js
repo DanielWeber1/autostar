aa58d8ac7661c632772f262407c69cb86e8dc5c1
WanielDeber <weber.dan@email.cz>
2025-03-15 17:06:11 +0100

    vypocet ceny podle vyberu

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ menu.classList.add("hidden");
 
 let correctPin;
 
+let price = 0;
+let transmission_price = 0;
+let color_price = 0;
+
 // Načtení hesla ze souboru
 fetch('secrets.json')
     .then(response => response.json())
@@ -49,6 +53,7 @@ window.addEventListener("click", function (event) {
     }
 });
 
+//posouvání obrázků ve slideshow pomocí šipek
 let currentSlide = 0;
     const slides = document.querySelectorAll(".slide");
 
@@ -57,3 +62,50 @@ let currentSlide = 0;
       currentSlide = (currentSlide + direction + slides.length) % slides.length;
       slides[currentSlide].classList.add("active");
     }
+
+
+
+/////////////////////////////////////////////
+//TUHLE ČÁST KÓDU BUDE MÍT KAŽDÉ AUTO JINAK//
+/////////////////////////////////////////////
+
+
+//změna ceny po výběru převodovky
+document.addEventListener("DOMContentLoaded", function () {
+    const radios = document.querySelectorAll('input[name="transmission-option"]');
+    const priceDisplay = document.getElementById("total-price");
+    
+    
+    radios.forEach(radio => {
+        radio.addEventListener("change", function () {
+            transmission_price = 0;
+            price = 500000;
+            transmission_price += parseInt(this.value, 10); // Přičteme hodnotu z radiobuttonu
+            price = price + color_price + transmission_price;
+            priceDisplay.textContent = price.toLocaleString("cs-CZ") + " Kč"; // Přepíšeme cenu
+        });
+    });
+});
+
+const redButton = document.getElementById("red");
+const silverButton = document.getElementById("silver");
+const blackButton = document.getElementById("black");
+const priceDisplay = document.getElementById("total-price");
+
+
+function updatePrice(event) 
+{
+    price = 500000;
+    color_price = 0;
+    color_price += parseInt(event.target.value, 10);
+    price = price + color_price + transmission_price;
+    priceDisplay.textContent = price.toLocaleString("cs-CZ") + " Kč";
+}
+
+// Přidáme event listener na všechna tlačítka
+redButton.addEventListener("click", updatePrice);
+silverButton.addEventListener("click", updatePrice);
+blackButton.addEventListener("click", updatePrice);
+
+
+
